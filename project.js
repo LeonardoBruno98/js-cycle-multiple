1dbf26a7d5158765a2ff8a8955b8ebb20a1a74a5
LeonardoBruno <leonardobruno98@hotmail.com>
2019-02-14 17:09:46 +0100

    fine

--- project.js
+++ project.js
@@ -1,11 +1,11 @@
-for (i=1; i < 100; i++) {
+for (i=0; i <= 100; i++) {
   if (i % 5 == 0) {
-    document.getElementById("mul5").innerHTML += i + "<br>";
+    document.getElementById("mul5").innerHTML += i ;
   }
   else if (i % 7 == 0) {
-    document.getElementById("mul7").innerHTML += i + "<br>";
+    document.getElementById("mul7").innerHTML += i ;
   }
-  else {
-    document.getElementById("other").innerHTML += i + "<br>";
+  if (i % 5 != 0 && i % 7 != 0){
+    document.getElementById("other").innerHTML += i;
   }
 }
